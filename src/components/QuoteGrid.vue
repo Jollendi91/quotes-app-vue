dbb963c95df3c96ad02698649e0a3c8b07ac2c64
Joshua Ollendick <joshua.ollendick@gmail.com>
2018-11-05 07:00:57 -0600

    implement add/delete quote

---
+++ src/components/QuoteGrid.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="row">
+        <single-quote v-for="(quote, index) in quotes" @click.native="deleteQuote(index)">{{ quote }}</single-quote>
+    </div>
+</template>
+
+<script>
+    import Quote from './Quote.vue';
+    export default {
+        props: ['quotes'],
+        components: {
+            singleQuote: Quote
+        },
+        methods: {
+            deleteQuote(index) {
+                this.$emit('quoteDeleted', index);
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
